cb6385b293a81860b05e1f109f6d2679a6d8b239
Muhammet Eren ÜNLÜ <muhammeterenunlu@gmail.com>
2023-05-22 01:14:22 +0300

    Update preprocessing.py file

--- preprocessing.py
+++ preprocessing.py
@@ -1,39 +1,45 @@
-
-from pandas.api.types import CategoricalDtype
 import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder, LabelEncoder
 
 def prepare_data(data):
-    # Define order for PRIORITY and URGENCY
-    priority_order = CategoricalDtype(['Trivial', 'Minor', 'Major', 'Critical', 'Blocker'], ordered=True)
-    urgency_order = CategoricalDtype(['Low', 'Medium', 'High'], ordered=True)
-
-    # Convert PRIORITY and URGENCY to ordinal (category)
-    data['PRIORITY'] = data['PRIORITY'].astype(priority_order)
-    data['URGENCY'] = data['URGENCY'].astype(urgency_order)
-
-    # Apply one hot encoding
-    data = pd.get_dummies(data, columns=['PRIORITY', 'URGENCY'])
-
-    # Define your ordinal columns
-    ordinal_boolean_columns = [
-        'PRIORITY_Trivial',
-        'PRIORITY_Minor',
-        'PRIORITY_Major',
-        'PRIORITY_Critical',
-        'PRIORITY_Blocker',
-        'URGENCY_Low',
-        'URGENCY_Medium',
-        'URGENCY_High',
+    columns_to_use = [
+        'PRIORITY',
+        'URGENCY',
+        'COMPONENT',
+        'IMPACT',
+        'ISSUE_CATEGORY',
+        'POZISYON',
+        'IS_BILGISI',
+        'ASSIGNEE',
+        'Total_Assignee',
+        'Total_Worklog_Assginee',
+        'Total_Log_Hours_Assignee',
+        'COMMENTOR_COUNT',
+        'COMMENT_COUNT',
+        'ISSUE_TYPE'
     ]
 
-    for column in ordinal_boolean_columns:
-        # Convert boolean to int
-        if data[column].dtype == 'bool':
-            data[column] = data[column].astype(int)
-        
-        # Check for missing values
+    data = data[columns_to_use].copy()  # Make a copy of the data
+
+    # Convert all columns to string type before encoding
+    for column in data.columns:
+        data[column] = data[column].astype(str)
+
+    # Check for missing values
+    for column in data.columns:
         if data[column].isnull().any():
             # Fill missing values with 0 or appropriate value
-            data[column] = data[column].fillna(0)
+            data[column] = data[column].fillna('0')
+
+    # Apply ordinal encoding
+    ordinal_encoder = OrdinalEncoder()
+    ordinal_columns = ['PRIORITY', 'URGENCY']
+    data[ordinal_columns] = ordinal_encoder.fit_transform(data[ordinal_columns])
+    
+    # Apply label encoding
+    label_encoder = LabelEncoder()
+    label_encoding_columns = ['COMPONENT', 'IMPACT', 'ISSUE_CATEGORY', 'POZISYON', 'IS_BILGISI', 'ASSIGNEE', 'ISSUE_TYPE']
+    for column in label_encoding_columns:
+        data[column] = label_encoder.fit_transform(data[column])
 
     return data
